0706707b9c46504aac385a5616adc2cfd5f948eb
shiv-konar <Balajipadmavati1>
2016-05-08 22:35:00 +0100

    Prevent resizing of the window

---
+++ 2.PreventResizing.py
@@ -0,0 +1,7 @@
+import tkinter as tk
+win = tk.Tk()  #Create an instance of the Tk class
+win.title("Python GUI")  #Set the title of the window
+
+win.resizable(0, 0)  #Disable resizing the GUI
+
+win.mainloop()  #The event which makes the window appear on screen
